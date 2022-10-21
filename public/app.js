91d2b36382e658cdf1671cafbfac0242770e98f9
starterdragon <papadie102@gmail.com>
2022-10-21 16:24:11 +0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // This will use the demo backend if you open index.html locally via file://, otherwise your server will be used
-let backendUrl = location.protocol === 'file:' ? "https://tiktok-chat-reader.zerody.one/" : undefined; //"https://tiktok-chat-reader.zerody.one/"; //
+let backendUrl = "https://tiktok-chat-reader.zerody.one/"; //location.protocol === 'file:' ? "https://tiktok-chat-reader.zerody.one/" : undefined; //
 let connection = new TikTokIOConnection(backendUrl);
 
 // Counter
